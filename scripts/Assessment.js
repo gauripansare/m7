8a91ed43359566fef86c328e4267f5b5929cc5e2
atul0030 <atul@knowdl.com>
2019-01-11 15:17:27 +0530

    update files

--- scripts/Assessment.js
+++ scripts/Assessment.js
@@ -294,7 +294,7 @@ var _Assessment = (function () {
 			}
 			if(_Navigator.IsPresenterMode())
 			{
-				$("#ScoreSummary").text("Score: 100%");
+				//$("#ScoreSummary").text("Score: 100%");
 				gRecordData.Status = "Completed";
 				
 			}
